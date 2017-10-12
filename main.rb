ca2c2e0fd3056de689b8129d249ebba965db84fa
Sidhra <sidhra.cader@hotmail.com>
2017-10-12 16:44:35 +1100

    map functionality

---
+++ main.rb
@@ -0,0 +1,11 @@
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+require 'pry'
+
+get '/' do
+  @search_carspots = HTTParty.get("https://data.melbourne.vic.gov.au/resource/dtpv-d4pf.json").parsed_response
+  # binding.pry
+  erb :index
+end
