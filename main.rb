1874181ae1e6b254b4897bd4088d644a64d45e19
Sidhra <sidhra.cader@hotmail.com>
2017-10-13 11:11:45 +1100

    working map api and location search

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ require 'pry'
 
 get '/' do
   @search_carspots = HTTParty.get("https://data.melbourne.vic.gov.au/resource/dtpv-d4pf.json").parsed_response
+  @search_carspots.delete_if{|carspot| carspot["status"] == "Present"}
   # binding.pry
+
   erb :index
 end
